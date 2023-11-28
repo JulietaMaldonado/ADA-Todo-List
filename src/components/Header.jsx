c02e225ef192f0fc2be0ea329c762e4097ee406a
Julieta Maldonado <80077281+JulietaMaldonado@users.noreply.github.com>
2023-11-27 21:39:18 -0300

    creado de header y agregado de estilos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,9 @@
+import styles from "./styles.module.css"
+
 export default function Header(){
     return <>
-    
+    <header className={styles.pageHeader}>
+        <h1 className={styles.pageTitle}>Todo List</h1>
+    </header>
     </>
 } 
