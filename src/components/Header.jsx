cc65536aa700488e9bf22499ad744a5476f544ef
Julieta Maldonado <80077281+JulietaMaldonado@users.noreply.github.com>
2023-11-29 19:22:33 -0300

    arreglo de header y uso de chakra

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import styles from "./styles.module.css"
+import { Heading } from '@chakra-ui/react'
 
 export default function Header(){
     return <>
-    <header className={styles.pageHeader}>
+    <Heading style={{height: "fit-content", padding: "10px", marginLeft: "auto", marginRight: "auto"}}>
         <h1 className={styles.pageTitle}>Todo List</h1>
-    </header>
+    </Heading>
     </>
 } 
